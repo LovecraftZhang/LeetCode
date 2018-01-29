2ee1819d5c9a1d56c68732786294cd08af4e1293
LovecraftZhang <lovecraft.zhang@gmail.com>
2018-01-29 14:25:40 -0800

    LeetCode

---
+++ AddTwoNumber_v2.java
@@ -0,0 +1,23 @@
+class AddTwoNumber_v2 {
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+			ListNode prev = new ListNode(0);
+			ListNode head = prev;
+			int carry = 0;
+			while (l1 != null || l2 != null || carry != 0) {
+				ListNode temp = new ListNode(0);
+				int sum = ((l2 == null) ? 0 : l2.val) + ((l1 == null) ? 0 : l1.val) + carry;
+				temp.val = sum % 10;
+				carry = sum / 10;
+				prev.next = temp;
+				prev = temp;
+				
+				l1 = (l1 == null) ? l1 : l1.next;
+				l2 = (l2 == null) ? l2 : l2.next;
+			}
+			return head.next;
+		}
+	
+	public static void main(String[] args) {
+		
+	}
+}
