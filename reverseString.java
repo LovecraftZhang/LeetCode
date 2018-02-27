ce962619259204c398b623c7312160f15242750a
LovecraftZhang <lovecraft.zhang@gmail.com>
2018-02-27 13:25:55 -0800

    LeetCode

---
+++ reverseString.java
@@ -0,0 +1,13 @@
+public String reverseString(String s) {
+        char[] result = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            char temp = result[i];
+            result[i] = result[j];
+            result[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(result);
+    }
