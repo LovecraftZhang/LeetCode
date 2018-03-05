40912617dbc42e6f105a430a48bb5090dfab6bf2
LovecraftZhang <lovecraft.zhang@gmail.com>
2018-03-05 14:39:10 -0800

    LeetCode

---
+++ reverseList.java
@@ -0,0 +1,8 @@
+    public ListNode reverseList(ListNode head) {
+       if(head==null || head.next==null) return head;
+        ListNode nextNode=head.next;
+        ListNode newHead=reverseList(nextNode);
+        nextNode.next=head;
+        head.next=null;
+        return newHead; 
+    }
